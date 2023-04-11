23bc683710ca07b59c2a174c09265b5a35325ecc
Rafinhaa <rafinhaa@me.com>
2023-04-11 15:39:41 -0300

    feat: Página SignIn

---
+++ src/pages/sign-in/index.page.tsx
@@ -0,0 +1,23 @@
+import { Typography } from "@/components/Typography";
+import { AuthButtons } from "@/components/AuthButtons";
+
+import { LogoSection, SignInSection, SingInContainer } from "./styles";
+
+const SignIn = () => {
+  return (
+    <SingInContainer>
+      <LogoSection />
+      <SignInSection>
+        <Typography.Heading size="lg" color="gray-100">
+          Boas vindas!
+        </Typography.Heading>
+        <Typography.Text color="gray-200">
+          Faça seu login ou acesse como visitante.
+        </Typography.Text>
+        <AuthButtons />
+      </SignInSection>
+    </SingInContainer>
+  );
+};
+
+export default SignIn;
---
+++ src/pages/sign-in/styles.ts
@@ -0,0 +1,33 @@
+import { styled } from "@/styles";
+
+export const SingInContainer = styled("main", {
+  display: "grid",
+  gridTemplateColumns: "1.1fr 1fr",
+  padding: 20,
+  minHeight: "100vh",
+});
+
+export const LogoSection = styled("section", {
+  width: "100%",
+  height: "100%",
+  background: "url(/images/logo-background.png) no-repeat center",
+  backgroundSize: "cover",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: 10,
+});
+
+export const SignInSection = styled("section", {
+  width: "100%",
+  maxWidth: "372px",
+  margin: "0 auto",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  paddingLeft: 20,
+
+  section: {
+    marginTop: 40,
+  },
+});
